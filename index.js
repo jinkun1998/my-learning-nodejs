d0e379bb81d21ec5dd1dd261f604489e69f6deb1
Quoc Thao <thaodlq2@fpt.com.vn>
2023-04-25 14:59:11 +0700

    Add append fn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path')
 
 fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8', (error, data) => {
     if (error)
-        throw error;
-    console.log(data);
+        throw error
+    console.log(data)
 });
 
 fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (error) => {
@@ -13,7 +13,13 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (e
     console.log('write complete')
 })
 
+fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes, it is.', (error) => {
+    if (error)
+        throw error
+    console.log('append complete')
+})
+
 process.on('uncaughtException', error => {
-    console.log(error);
-    process.exit(1);
-});
+    console.log(error)
+    process.exit(1)
+})
