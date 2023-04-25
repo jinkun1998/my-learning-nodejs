f0ff980a88331ea9ba625a0c202f5e12c00a16c7
Quoc Thao <thaodlq2@fpt.com.vn>
2023-04-25 14:50:26 +0700

    Change path string to path library

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
+const path = require('path')
 
-fs.readFile('./files/stater.txt', 'utf-8', (error, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8', (error, data) => {
     if (error)
         throw error;
     console.log(data);
-})
+});
 
 process.on('uncaughtException', error => {
     console.log(error);
     process.exit(1);
-})
+});
