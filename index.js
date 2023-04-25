9278c94587136958b18a50f524040f76583f0cce
Quoc Thao <thaodlq2@fpt.com.vn>
2023-04-25 14:45:50 +0700

    Add event on uncaughtException

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile('./files/stater.txt', 'utf-8', (error, data) => {
     if (error)
-        throw error
-    console.log(data)
+        throw error;
+    console.log(data);
+})
+
+process.on('uncaughtException', error => {
+    console.log(error);
+    process.exit(1);
 })
