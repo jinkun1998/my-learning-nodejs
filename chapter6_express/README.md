5cd59151fc526d00aa4eb9f9de481bc990cae08f
Quoc Thao <thaodlq2@fpt.com.vn>
2023-04-27 14:18:29 +0700

    add middleware and apply it

--- chapter6_express/README.md
+++ chapter6_express/README.md
@@ -7,7 +7,7 @@
 - const exporess = require('express')
 - const app = express()
 - app.get/post/put/delete/all/...
-- example:
+  example:
   app.get('/', (req, res) => {
   //TODO: process data here
   })
@@ -17,4 +17,12 @@
   next()
   }, (req, res) => {
   // TODO: process data here
+  })
+
+- apply middleware:
+  app.get('/middleware', [<middleware 1>, <middleware 2>, ...])
+
+- all exception route must be defined in the end of router register
+  app.get('/\*', (req, res) => {
+  res.status(404).send('<h1>there is nothing to do</h1>')
   })
--- chapter6_express/server.js
+++ chapter6_express/server.js
@@ -10,6 +10,25 @@ app.get('/about(.html)?', (req, res) => {
     res.status(404).send('<h1>this is about page</h1>')
 })
 
+// define middleware
+const one = (req, res, next) => {
+    console.log('middleware 1')
+    next()
+}
+
+const two = (req, res, next) => {
+    console.log('middleware 2')
+    next()
+}
+
+const three = (req, res, next) => {
+    console.log('middleware 3')
+    res.send('<h1>finished middleware</h1>')
+}
+
+// apply middleware
+app.get('/middleware', [one, two, three])
+
 app.get('/*', (req, res) => {
     res.status(404).send('<h1>there is nothing to do</h1>')
 })
