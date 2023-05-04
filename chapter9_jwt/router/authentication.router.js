73ad39f77cde385b58480b4228b43928cd51ca40
Quoc Thao <thaodlq2@fpt.com.vn>
2023-05-04 08:41:23 +0700

    init chapter9_jwt

---
+++ chapter9_jwt/router/authentication.router.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const authenticationController = require('../controllers/authentication.controller')
+
+router
+    .post('/login', authenticationController.login)
+    .post('/register', authenticationController.register)
+
+module.exports = router
